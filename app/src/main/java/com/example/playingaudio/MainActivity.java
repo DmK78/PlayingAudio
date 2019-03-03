280f8353bbab24fa49eac6dd77e8ae912036efad
DmK78 <Ek193burg>
2019-03-03 14:04:09 +0500

    final

--- app/src/main/java/com/example/playingaudio/MainActivity.java
+++ app/src/main/java/com/example/playingaudio/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.playingaudio;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    boolean isPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.malbek);
+
+
     }
+
+    public void play(View view) {
+        Button button = findViewById(R.id.playButton);
+        if (!isPlaying) {
+            mediaPlayer.start();
+            button.setText("Pause");
+            isPlaying=true;
+        } else {
+            mediaPlayer.pause();
+            button.setText("Play");
+            isPlaying=false;
+        }
+
+    }
+
+
 }
